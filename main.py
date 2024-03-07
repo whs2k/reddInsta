51cd0be8b996683f00c9557153a04768c54690b3
Will Solomon <whs2k@virginia.edu>
2024-03-06 22:03:16 -0800

    Update main.py

--- main.py
+++ main.py
@@ -19,26 +19,26 @@ print('num of arguments: ', len(sys.argv))
 input_args = sys.argv
 
 list_of_subreddits = ['MikeAdriano','AngelaWhite', 'RemyLaCroix','GiannaMichaels',
-                      'girlsfinishing','cumsluts','javdreams',
-                     'bukkake','PornIsCheating','pornrelapsed',
-                     'ClassyPornstars','ModelsGoneMild','AvaAddams',
-                     'RileyReid','SavannahBond','tiktokthots','GirlsFinishingTheJob',
-                     'GoonetteHub']
+					  'girlsfinishing','cumsluts','javdreams',
+					 'bukkake','PornIsCheating','pornrelapsed',
+					 'ClassyPornstars','ModelsGoneMild','AvaAddams',
+					 'RileyReid','SavannahBond','tiktokthots','GirlsFinishingTheJob',
+					 'GoonetteHub']
 
 reddit = praw.Reddit(client_id=input_args[1], #REDDIT_CLIENT_ID
-                     client_secret=input_args[2],#REDDIT_CLIENT_SECRET
-                     password=input_args[3], #REDDIT_PASSWORD
-                     user_agent=input_args[4], #REDDIT_USER_AGENT
-                     username=input_args[5] #REDDIT_USER_NAME
-                     )
+					 client_secret=input_args[2],#REDDIT_CLIENT_SECRET
+					 password=input_args[3], #REDDIT_PASSWORD
+					 user_agent=input_args[4], #REDDIT_USER_AGENT
+					 username=input_args[5] #REDDIT_USER_NAME
+					 )
 twitter_api_authorized = Api(
-        access_token=input_args[6], #TWITTER_ACCESS_TOKEN,
-        access_secret=input_args[7], #TWITTER_ACCESS_TOKEN_SECRET
-        client_id = '1712104648881192960',
-        consumer_key = input_args[8], #TWITTER_CONSUMER_KEY
-        consumer_secret = input_args[9], #TWITTER_CONSUMER_SECRET
-    oauth_flow=True
-    )
+		access_token=input_args[6], #TWITTER_ACCESS_TOKEN,
+		access_secret=input_args[7], #TWITTER_ACCESS_TOKEN_SECRET
+		client_id = '1712104648881192960',
+		consumer_key = input_args[8], #TWITTER_CONSUMER_KEY
+		consumer_secret = input_args[9], #TWITTER_CONSUMER_SECRET
+	oauth_flow=True
+	)
 
 
 
@@ -48,18 +48,18 @@ print(subreddit)
 for x in reddit.subreddit(subreddit).top(time_filter='day',limit=25):
 	print(x.url)
 	if 'redgifs' in x.url:
-        url = x.url
-        print(url)
-        #resp = requests.get(url) # making requests to server
-        #with open(filename, "wb") as f: # opening a file handler to create new file 
-        #    f.write(resp.content) # writing content to file
-        red_gifs_api = API() #redgifs.API()
-        time.sleep(2)
-        red_gifs_api.login()
-        red_gifs_api.download(url, filename)
-        
-        tweet_title=x.title
-        break
+		url = x.url
+		print(url)
+		#resp = requests.get(url) # making requests to server
+		#with open(filename, "wb") as f: # opening a file handler to create new file 
+		#    f.write(resp.content) # writing content to file
+		red_gifs_api = API() #redgifs.API()
+		time.sleep(2)
+		red_gifs_api.login()
+		red_gifs_api.download(url, filename)
+		
+		tweet_title=x.title
+		break
 
 clip = VideoFileClip(filename)
 duration = clip.duration
@@ -71,28 +71,28 @@ final_file_size = seconds_per_chunk*num_of_chunks
 print(duration, seconds_per_chunk, num_of_chunks, final_file_size)
 lines = ''
 for x in range(num_of_chunks):
-    lines = lines + str(round(x*seconds_per_chunk,3)) + '-' + str(round((x+1)*seconds_per_chunk,3)) + '\n'
+	lines = lines + str(round(x*seconds_per_chunk,3)) + '-' + str(round((x+1)*seconds_per_chunk,3)) + '\n'
 
 print(lines)
 with open("times.txt", "w") as text_file:
-    text_file.write(lines)
+	text_file.write(lines)
 
 # Replace the filename below.
 required_video_file = 'to_upload.mp4'
 
 with open("times.txt") as f:
-    times = f.readlines()
+	times = f.readlines()
 
 times = [x.strip() for x in times] 
 dir_name = 'vid_chunks'
 try:
-    os.mkdir(dir_name)
+	os.mkdir(dir_name)
 except:
-    pass
+	pass
 for time_ in times:
-    starttime = float(time_.split("-")[0])
-    endtime = float(time_.split("-")[1])
-    ffmpeg_extract_subclip(required_video_file, starttime, endtime, targetname='vid_chunks/'+str(times.index(time_)+1)+".mp4")
+	starttime = float(time_.split("-")[0])
+	endtime = float(time_.split("-")[1])
+	ffmpeg_extract_subclip(required_video_file, starttime, endtime, targetname='vid_chunks/'+str(times.index(time_)+1)+".mp4")
 
 video_parts = os.listdir('vid_chunks')
 video_parts = list(set(['vid_chunks/'+x for x in video_parts]))
@@ -102,25 +102,25 @@ print(video_parts)
 final_size = 0
 part_size = os.stat(video_parts[0]).st_size 
 for x in video_parts:
-    final_size = final_size+os.stat(x).st_size 
-    
+	final_size = final_size+os.stat(x).st_size 
+	
 print(final_size)
 print(os.stat(video_parts[0]).st_size)
 
 resp = twitter_api_authorized.upload_media_chunked_init(
-    total_bytes=final_size,
-    media_type="video/mp4",
+	total_bytes=final_size,
+	media_type="video/mp4",
 )
 media_id = resp.media_id_string
 
 for idx, part in enumerate(video_parts):
-    with open(part, "rb") as media:
-        status = twitter_api_authorized.upload_media_chunked_append(
-            media_id=media_id,
-            media=media,
-            segment_index=idx,
-        )
-        print(part, status)
+	with open(part, "rb") as media:
+		status = twitter_api_authorized.upload_media_chunked_append(
+			media_id=media_id,
+			media=media,
+			segment_index=idx,
+		)
+		print(part, status)
 
 resp = twitter_api_authorized.upload_media_chunked_finalize(media_id=media_id)
 print(resp)
@@ -131,8 +131,8 @@ resp = twitter_api_authorized.upload_media_chunked_status(media_id=media_id)
 print(resp)
 
 twitter_api_authorized.create_tweet(
-    text=tweet_title,
-    media_media_ids=[media_id],
+	text=tweet_title,
+	media_media_ids=[media_id],
 )
 
 os.remove(filename)
